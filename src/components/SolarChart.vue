09372e7d9df66795153b73a3519184b547f5d3f7
Simone-Pagotto <simone.pagotto.kg@gmail.com>
2021-03-29 17:13:37 +0200

    reactivity not working

--- src/components/SolarChart.vue
+++ src/components/SolarChart.vue
@@ -1,11 +1,10 @@
 <script>
 /* eslint-disable */
-import { Line, mixins } from "vue-chartjs";//
-//const { reactiveProp } = mixins;
-
+import { Line,mixins } from "vue-chartjs";//
+const { reactiveProp } = mixins;
 export default {
     extends: Line,
-    mixins: [mixins.reactiveProp],
+    mixins: [reactiveProp],
     props: {
         label: {
             type: String,
@@ -17,29 +16,22 @@ export default {
             type: Object,
         }
     },
-    data() {
-        return {
-            chart:null,
-        }
-    },
-    watch: {
-        datasets(newDatasets) {
-            console.log(newDatasets);
-            this.chart.datasets = newDatasets;
-            this.chart.update
-        }
-    },
     mounted () {
-        let hours = this.chartData.map(d => d.hour);
-        let performance = this.chartData.map(d => d.yield);
+        let hours=[];
+        let performance=[];
+        console.log(this.chartData);
+        if(this.chartData.length > 0){
+            
+            hours = this.chartData.map(d => d.hour);
+            performance = this.chartData.map(d => d.yield);
+        }
+        
         this.renderChart({
             labels: hours,
             datasets: [{
-                label: this.label,
                 data: performance,
             }],
         },this.options);
-    },
-    
+    }
 }
 </script>
